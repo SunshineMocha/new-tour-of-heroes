b2ec00c9be9880da1081ea298bc3eca9b8a6322e
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-05-11 20:02:01 +0200

    Made 5/6 in the tutorial

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from 'src/app/model/hero';
+import { HeroService } from 'src/app/services/hero/hero.service';
+import { MessageService } from 'src/app/services/message/message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  // hero : Hero = {
+  //   id: 1,
+  //   name: 'Mocha',
+  //};
+
+
+    // selectedHero?: Hero;
+
+    heroes: Hero[] = [];
+
+    constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+    ngOnInit(): void {
+      this.getHeroes();
+    }
+
+
+    // onSelect(hero: Hero): void {
+    //   this.selectedHero = hero;
+    //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    // }
+
+    getHeroes(): void {
+      this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+    }
+    
+}
